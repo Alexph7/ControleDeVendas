cb2ed135892d8d2772aa936036c9e0d33fbff4b4
ph757 <ph757@Fabia>
2022-11-04 10:12:39 -0300

    Criar Metodo Pesquisar
    Metodo para Pesquisar Usuario do banco de dados e mostrar em interface

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -55,4 +55,29 @@ public class UsuarioDAO {
          JOptionPane.showMessageDialog(null, "UsuariDAO Metodo Cadastrar: " + e);
       }
    }
+
+   public ArrayList<UsuarioDTO> pesquisarUsuario() {
+      String sql = "select * from usuario";
+
+      try {
+         pstm = conexao.prepareStatement(sql);
+         rs = pstm.executeQuery();
+
+         while (rs.next()) {
+            UsuarioDTO objUsuarioDTO = new UsuarioDTO();
+            objUsuarioDTO.setIdUser(rs.getInt(1));
+            objUsuarioDTO.setNome(rs.getString(2));
+            objUsuarioDTO.setLogin(rs.getString(3));
+            objUsuarioDTO.setSenha(rs.getString(4));
+            objUsuarioDTO.setFone(rs.getString(5));
+            objUsuarioDTO.setNivelDeAcesso(rs.getString(6));
+
+            lista.add(objUsuarioDTO);
+
+         }
+      } catch (SQLException e) {
+         JOptionPane.showMessageDialog(null, "UsuariDAO Metodo Pesquisar: " + e);
+      }
+      return lista;
+   }
 }
