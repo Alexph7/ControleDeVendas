b93a7605c3ef1a3911c6dbd9536381a2f2c07778
Alexph7 <alexph7.56@gmail.com>
2022-11-04 09:32:03 -0300

    Criar Metodo Conector()
    Metodo para Conectar Java Com Mysql

---
+++ src/DAO/ConexaoDAO.java
@@ -0,0 +1,26 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ph757
+ */
+public class ConexaoDAO {
+
+   public static Connection conector() {
+
+      Connection conexao = null;
+      try {
+
+         conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/controledevendas", "root", "");
+
+      } catch (SQLException e) {
+         JOptionPane.showMessageDialog(null, "ConexaoDAO conector()" + e);
+      }
+      return conexao;
+   }
+}
